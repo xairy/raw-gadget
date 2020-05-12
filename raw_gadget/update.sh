9ef578bc4c5217130851d84da6ede194250e50c8
Andrey Konovalov <andreyknvl@gmail.com>
2020-05-12 19:16:22 +0200

    raw_gadget: update module
    Currently includes some non-yet-mainlined patches.

--- raw_gadget/update.sh
+++ raw_gadget/update.sh
@@ -2,7 +2,10 @@
 
 set -eux
 
-REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain
+# TODO: swtich back to mainline, once all the fixes are there.
+# REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain
+
+REPO=https://raw.githubusercontent.com/xairy/linux/raw-gadget
 
 wget $REPO/drivers/usb/gadget/legacy/raw_gadget.c -O raw_gadget.c
 wget $REPO/include/uapi/linux/usb/raw_gadget.h -O raw_gadget.h
