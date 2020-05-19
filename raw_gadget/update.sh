d9e7ed71a5862c574b1001f0d58d123bc9c942cf
Andrey Konovalov <andreyknvl@gmail.com>
2020-05-19 22:59:41 +0200

    raw_gadget: update

--- raw_gadget/update.sh
+++ raw_gadget/update.sh
@@ -2,10 +2,7 @@
 
 set -eux
 
-# TODO: swtich back to mainline, once all the fixes are there.
-# REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain
-
-REPO=https://raw.githubusercontent.com/xairy/linux/raw-gadget
+REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain
 
 wget $REPO/drivers/usb/gadget/legacy/raw_gadget.c -O raw_gadget.c
 wget $REPO/include/uapi/linux/usb/raw_gadget.h -O raw_gadget.h
