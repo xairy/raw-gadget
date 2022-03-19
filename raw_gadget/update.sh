c5baceabbe6cceb8e6ac85211c7ac6ee5b28c4bc
Andrey Konovalov <andreyknvl@gmail.com>
2022-03-19 21:24:12 +0100

    raw_gadget: update module from usb-next
    Closes #23.
    
    Thanks to @AristoChen for the kernel patch.

--- raw_gadget/update.sh
+++ raw_gadget/update.sh
@@ -3,9 +3,10 @@
 
 set -eux
 
-REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain
+REPO=https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usb.git/plain
+BRANCH=usb-next
 
-wget $REPO/drivers/usb/gadget/legacy/raw_gadget.c -O raw_gadget.c
-wget $REPO/include/uapi/linux/usb/raw_gadget.h -O raw_gadget.h
+wget $REPO/drivers/usb/gadget/legacy/raw_gadget.c?h=$BRANCH -O raw_gadget.c
+wget $REPO/include/uapi/linux/usb/raw_gadget.h?h=$BRANCH -O raw_gadget.h
 
 git apply ./include.patch
