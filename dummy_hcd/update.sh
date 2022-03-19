ce0ac3fa81f8c32176dcceec5858a2e22f222f5f
Andrey Konovalov <andreyknvl@gmail.com>
2022-03-19 21:18:58 +0100

    dummy_hcd: revert breaking commit

--- dummy_hcd/update.sh
+++ dummy_hcd/update.sh
@@ -10,3 +10,7 @@ wget $REPO/drivers/usb/gadget/udc/dummy_hcd.c -O dummy_hcd.c
 # This patch is needed in case the kernel you're building against doesn't have
 # commit 4d537f37e0d39 ("usb: introduce usb_ep_type_string() function").
 git apply ./usb_ep_type_string.patch
+
+# This patch is needed in case the kernel you're building against doesn't have
+# commit 7dc0c55e9f30 ("USB: UDC core: Add udc_async_callbacks gadget op").
+git apply ./usb_udc_async_callbacks.patch
