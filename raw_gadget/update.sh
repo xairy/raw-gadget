8274ee02badc8804e63cca41ed24fbc223ff69f9
Andrey Konovalov <andreyknvl@gmail.com>
2022-12-08 23:10:38 +0100

    raw_gadget: update module
    Keep Raw Gadget compatible with older kernels: revert the change done by
    upstream commit af1969a2d734 ("USB: gadget: Rename usb_gadget_probe_driver()").

--- raw_gadget/update.sh
+++ raw_gadget/update.sh
@@ -9,4 +9,8 @@ BRANCH=usb-next
 wget $REPO/drivers/usb/gadget/legacy/raw_gadget.c?h=$BRANCH -O raw_gadget.c
 wget $REPO/include/uapi/linux/usb/raw_gadget.h?h=$BRANCH -O raw_gadget.h
 
-git apply ./include.patch
+# Fix up include names for local usage.
+git apply ./patches/include.patch
+
+# Fix up usb_gadget_probe_driver rename introduced by af1969a2d734 (23 Apr 2022).
+git apply ./patches/usb_gadget_probe_driver.patch
