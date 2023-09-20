ffda393ef77b6d5c1e3265c697d74b5eb54cc34d
Andrey Konovalov <andreyknvl@gmail.com>
2023-09-20 02:15:37 +0200

    raw_gadget, dummy_hcd: update modules

--- dummy_hcd/update.sh
+++ dummy_hcd/update.sh
@@ -9,8 +9,13 @@ wget $REPO/drivers/usb/gadget/udc/dummy_hcd.c -O dummy_hcd.c
 
 # This patch is needed in case the kernel you're building against doesn't have
 # commit 4d537f37e0d39 ("usb: introduce usb_ep_type_string() function").
-git apply ./usb_ep_type_string.patch
+git apply ./patches/usb_ep_type_string.patch
 
 # This patch is needed in case the kernel you're building against doesn't have
 # commit 7dc0c55e9f30 ("USB: UDC core: Add udc_async_callbacks gadget op").
-git apply ./usb_udc_async_callbacks.patch
+git apply ./patches/dummy_udc_async_callbacks.patch
+
+# This patch is needed in case the kernel you're building against doesn't have
+# commit 2dd3f64fcc11 ("usb: gadget/dummy_hcd: Convert to platform remove
+# callback returning void").
+git apply ./patches/dummy_driver_remove_new.patch
